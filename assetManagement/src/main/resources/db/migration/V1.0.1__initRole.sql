021b206d57973af856d243700c620dab7bb7fcb2
rnielso1 <49373388+rnielso1@users.noreply.github.com>
2019-04-19 13:32:52 -0500

    Edited Role Table

--- assetManagement/src/main/resources/db/migration/V1.0.1__initRole.sql
+++ assetManagement/src/main/resources/db/migration/V1.0.1__initRole.sql
@@ -1,10 +1,10 @@
 DROP TABLE Role;
 
 CREATE TABLE Role(
-	role_id INTEGER,
-	employee_id INTEGER,
+	role_id Number,
+	employee_id Number,
 	description VARCHAR(25),
-	access_level INTEGER(1),
+	access_level Number,
 	PRIMARY key (role_id),
 	FOREIGN KEY (employee_id)
 		REFERENCES employee(employee_id)
